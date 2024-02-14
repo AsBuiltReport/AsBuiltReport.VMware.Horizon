9b9e199ead78d58e87ad92e04f6c41eafbed80f3
Jonathan Colon <rebelinux@gmail.com>
2024-02-14 09:07:44 -0400

    Update Get-RequiredModule.ps1

--- Src/Private/Get-RequiredModule.ps1
+++ Src/Private/Get-RequiredModule.ps1
@@ -21,14 +21,16 @@ function Get-RequiredModule {
         [ValidateNotNullOrEmpty()]
         [String] $Version
     )
-
-    # Check if the required version of VMware PowerCLI is installed
-    $RequiredModule = Get-Module -ListAvailable -Name $Name | Sort-Object -Property Version -Descending | Select-Object -First 1
-    $ModuleVersion = "$($RequiredModule.Version.Major)" + "." + "$($RequiredModule.Version.Minor)"
-    if ($ModuleVersion -eq ".") {
-        throw "VMware PowerCLI $Version or higher is required to run the VMware Horizon As Built Report. Run 'Install-Module -Name $Name -MinimumVersion $Version' to install the required modules."
+    process {
+        # Check if the required version of VMware PowerCLI is installed
+        $RequiredModule = Get-Module -ListAvailable -Name $Name | Sort-Object -Property Version -Descending | Select-Object -First 1
+        $ModuleVersion = "$($RequiredModule.Version.Major)" + "." + "$($RequiredModule.Version.Minor)"
+        if ($ModuleVersion -eq ".") {
+            throw "VMware PowerCLI $Version or higher is required to run the VMware Horizon As Built Report. Run 'Install-Module -Name $Name -MinimumVersion $Version' to install the required modules."
+        }
+        if ($ModuleVersion -lt $Version) {
+            throw "VMware PowerCLI $Version or higher is required to run the VMware Horizon As Built Report. Run 'Update-Module -Name $Name -MinimumVersion $Version' to update the required modules."
+        }
     }
-    if ($ModuleVersion -lt $Version) {
-        throw "VMware PowerCLI $Version or higher is required to run the VMware Horizon As Built Report. Run 'Update-Module -Name $Name -MinimumVersion $Version' to update the required modules."
-    }
+    end {}
 }
